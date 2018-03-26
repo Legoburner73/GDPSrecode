0c4adcf37cce0ed9ab96386d4da1722536e60690
Legoburner73 <legoplaysmcmail@gmail.com>
2018-03-26 10:01:08 +0100

    Update acceptGJFriendRequest20.php

--- acceptGJFriendRequest20.php
+++ acceptGJFriendRequest20.php
@@ -1,3 +1,8 @@
 <?php
-//Friend Accepted Code
+chdir(dirname(__FILE__));
+include "../lib/connection.php";
+include "../lib/GJPCheck.php";
+require_once "../lib/antiExploit.php";
+$antiExploit = new antiExploit();
+if(empty($_POST["gjp"])
 ?>
